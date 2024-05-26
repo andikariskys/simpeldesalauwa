e5cd5d069d55c855c5be10947de0c4ddd6f5af7b
sayyidazizii <sayyidsyafiq234@gmail.com>
2024-05-26 10:47:29 +0700

    feat(navbar.php): add name pass

--- application/views/service/templates/navbar.php
+++ application/views/service/templates/navbar.php
@@ -95,7 +95,7 @@
                      </div>
                      <div class="form-group">
                          <label for="exampleInputPassword1">Password</label>
-                         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                         <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                      </div>
                      <button type="submit" class="btn btn-secondary">Login</button>
                  </form>
